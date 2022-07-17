bbdc60d172ddba0fedf891b87ef94ac1ab4fbcb5
devgis <devgis@163.com>
2022-07-17 16:21:28 +0800

    V0
    V0

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # BDCloundDiskEnhancement
- 自当重启百度网盘让百度网盘恢复高速下载工具
+
+## 说明
+![Alt](Pictures/0.png)
+![Alt](Pictures/1.png)
+![Alt](Pictures/2.png)
+![Alt](Pictures/4.png)
+````
+配置百度网盘优化模式可以解除限速，但是运行一段时间后又会重新降速，重启百度网盘后速度又恢复。
+所以编写这个程序后台自动定时重新启动百度网盘,就可以提高限制速度，程序代码中主要有一下几个配置信息需要修改：
+processNames 百度网盘的进程，理论上不需要修改;
+startExePath 百度网盘exe的地址 默认应该是  @"C:\Users\Administrator\AppData\Roaming\baidu\BaiduNetdisk\BaiduNetdisk.exe"，具体修改为你的就可以;
+StartInterval = 5000; //结束到启动间隔毫秒
+RestartInterval = 600000; //重启间隔毫秒
+注意要把你的百度网盘账号设置为自动登录。
+本代码使用C# 和 .net core 5.0编写，开发环境VS2019你可以按你的需求修改为其他平台。
+```
---
+++ Source/BDCloundDiskEnhancement/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace BDCloundDiskEnhancement
+{
+    class Program
+    {
+        static List<string> processNames = new List<string> { "baidunetdisk", "baidunetdiskhost" };
+        static string startExePath = @"C:\Users\Administrator\AppData\Roaming\baidu\BaiduNetdisk\BaiduNetdisk.exe";
+        const int StartInterval = 5000; //结束到启动间隔毫秒
+        const int RestartInterval = 600000; //间隔毫秒
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started!");
+            int i = 1;
+            while (true)
+            {
+                try {
+                    //kill old
+                    foreach (var p in processNames)
+                    {
+                        KillProcess(p);
+                    }
+
+                    Thread.Sleep(StartInterval);
+                    //start new
+                    System.Diagnostics.Process.Start(startExePath);
+                    Console.WriteLine($"RestartedStarted{i}");
+
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Error:"+ex.Message);
+                }
+                Thread.Sleep(RestartInterval);
+            }
+        }
+
+        static void KillProcess(string processName)
+        {
+            System.Diagnostics.Process[] process = System.Diagnostics.Process.GetProcessesByName(processName);
+            foreach (System.Diagnostics.Process p in process)
+            {
+                p.Kill();
+            }
+        }
+    }
+}
